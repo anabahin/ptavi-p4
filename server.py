11b25bbbd44d2847ffc25a48ee9b7da195a4d442
Nur Nabahin <nurnabahin@MacBook-Pro-de-Nur.local>
2018-10-27 12:14:33 +0200

    subo el apartado 5 en el que el servidor recibe IP y Puerto del cliente y s ele pasa el puerto al servidor

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+
+try:
+    PORT = sys.argv[1]
+except IndexError:
+    sys.exit("Usage: python3 server.py <port>")
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -17,14 +23,18 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
+        IP = self.client_address[0]
+        PORT = self.client_address[1]
+        print("client_ip: ", IP + "\t", "client_port: ", PORT)
         self.wfile.write(b"Hemos recibido tu peticion")
+        
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            print("El cliente nos manda: ", line.decode('utf-8'))
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', int(PORT)), EchoHandler) 
 
     print("Lanzando servidor UDP de eco...")
     try:
