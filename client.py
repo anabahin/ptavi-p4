6f4ce64b019e6e95aa3518cf7c731f56d7150f7d
Nur Nabahin <nurnabahin@MacBook-Pro-de-Nur.local>
2018-10-27 11:50:37 +0200

    subo el apartado 4 los parametros IP puerto y texto introducidos desde el shell

--- client.py
+++ client.py
@@ -5,17 +5,29 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+#paso la IP y puerto y el comentario
+try:
+    SERVER = sys.argv[1]
+    PORT = sys.argv[2]
+    COMMENT = sys.argv[3]
+    
+    if len(sys.argv) > 3:
+        COMMENT = ''
+        CADENA = sys.argv[3:]
+        for caracteres in CADENA:
+            COMMENT += ' ' + caracteres
+            
+except IndexError:
+    sys.exit(" Usage: python3 client.py <server> <port> <Comment>")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.connect((SERVER, int(PORT)))
+    print("Enviando:", COMMENT)
+    my_socket.send(bytes(COMMENT, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
