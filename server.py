2b46a4d41e52e16c55addfd90c6d8fa2ba72ac6a
Nur Nabahin <nurnabahin@MacBook-Pro-de-Nur.local>
2018-10-28 19:11:51 +0100

    punto 8 register2json

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+from time import gmtime, strftime, time
+import json
 
 try:
     PORT = sys.argv[1]
@@ -18,26 +20,46 @@ class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     dic = {}
+    
+    def register2json(self):
+        json.dump(self.dic, open('registered,json', 'w'))
+
+
+    def json2registered(self):
+        try:
+            with open('registered.json', 'r') as file:
+                self.dic = json.load(file)
+                self.expiration()
+        except:
+            pass
+    def expiration(self):
+        expired = []
+        time_exp = strftime('%Y-%m-%d %H:%M:%S', gmtime(time()))
+        for user in self.dic:
+            if self.dic[user][1] <= time_exp:
+                expired.append(user)
+        for user in expired:
+            del self.dic[user]
+    
     def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        IP = self.client_address[0]
-        PORT = self.client_address[1]
-        print("client_ip: ", IP + "\t", "client_port: ", PORT)
+        self.expiration()
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         print(self.client_address)
         for line in self.rfile:
             if line:
                 if line.decode('utf-8')[:8] == 'REGISTER':
                     user = line.decode('utf-8')[13:-10]
-                    self.dic[user] = self.client_address[0]
+                    ip = self.client_address[0]
                 elif line.decode('utf-8')[:7] == 'Expires':
-                    if line.decode('utf-8').split(' ')[1][0] == '0':
+                    expires = int(line.decode('utf-8')[9:]) + time()
+                    expire = strftime('%Y-%m-%d %H:%M:%S', gmtime(expires))
+                    if line.decode('utf-8').split(' ')[1][0] != '0':
+                        self.dic[user] = [ip, expire]
+                    else:
                         del self.dic[user]
             print(line.decode('utf-8'),end='')
         print(self.dic)
+        self.register2json()
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
