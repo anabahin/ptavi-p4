cb6a15f406386f624cd2372a32a1a9019dccb555
Ali Nabahin <anabahin@beta09.aulas.gsyc.urjc.es>
2018-10-30 16:19:31 +0100

    subo clienty.py con pycodestyle y pep257

--- client.py
+++ client.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
+"""Programa cliente UDP que abre un socket a un servidor."""
 
 import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 # paso la IP y puerto y el comentario
-if len(sys.argv) < 5:
-    sys.exit("Usage: python3 client.py <ip> <port> \
-             <register> <sip_address> <expires_value>")
+if len(sys.argv) < 6:
+    sys.exit("Usage: python3 client.py < ip > < port > "
+             " < register > < sip_address > < expires_value >")
 
 SERVER = sys.argv[1]
 PORT = sys.argv[2]
@@ -24,8 +22,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     print("Enviando:", COMMENT)
     if sys.argv[3] == 'register':
         my_socket.send(bytes('REGISTER sip: ' + COMMENT +
-                             + '  SIP/2.0\r\nExpires: ' +
-                             + EXPIRE + '\r\n\r\n', 'utf-8') + b'\r\n')
+                             '  SIP/2.0\r\nExpires: ' +
+                             EXPIRE + '\r\n\r\n', 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
