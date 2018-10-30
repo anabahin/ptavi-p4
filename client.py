9253066c4fa90c796a043094c4c1d477b874fe24
Ali Nabahin <anabahin@alpha48.aulas.gsyc.urjc.es>
2018-10-30 13:12:27 +0100

    subo client.py con pycodestyle

--- client.py
+++ client.py
@@ -8,21 +8,24 @@ import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-#paso la IP y puerto y el comentario
-if len(sys.argv)<5:
-    sys.exit("Usage: python3 client.py <ip> <port> <register> <sip_address> <expires_value>")
+# paso la IP y puerto y el comentario
+if len(sys.argv) < 5:
+    sys.exit("Usage: python3 client.py <ip> <port> \
+             <register> <sip_address> <expires_value>")
 
 SERVER = sys.argv[1]
 PORT = sys.argv[2]
-COMMENT =(sys.argv[4])
+COMMENT = (sys.argv[4])
 EXPIRE = sys.argv[5]
-            
+
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, int(PORT)))
     print("Enviando:", COMMENT)
     if sys.argv[3] == 'register':
-        my_socket.send(bytes('REGISTER sip: '+COMMENT+'  SIP/2.0\r\nExpires: ' +EXPIRE+'\r\n\r\n','utf-8') + b'\r\n')
+        my_socket.send(bytes('REGISTER sip: ' + COMMENT +
+                             + '  SIP/2.0\r\nExpires: ' +
+                             + EXPIRE + '\r\n\r\n', 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
